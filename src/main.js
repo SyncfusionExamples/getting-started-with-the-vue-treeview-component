8d1ba95b945b8879cb2cbe3f841f12d82e16b015
MeenaAlagiah <117711147+MeenaAlagiah@users.noreply.github.com>
2023-07-28 20:21:13 +0530

    Committing the Vue TreeView Getting Started sample

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { registerLicense } from '@syncfusion/ej2-base';
+registerLicense("ORg4AjUWIQA/Gnt2V1hhQlJNfVddX2BWfFN0RnNYfVR0d19DZUwxOX1dQl9gSXlRckRhXXtdd3xTRWU=");
+createApp(App).mount('#app')
